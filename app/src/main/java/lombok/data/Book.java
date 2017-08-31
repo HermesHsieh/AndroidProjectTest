c0af59a63b0992a13917467424e2a7a6dbde6d6b
Hermes <hermes.hsien@gmail.com>
2017-08-31 21:14:03 +0800

    add lombok lib.

---
+++ app/src/main/java/lombok/data/Book.java
@@ -0,0 +1,14 @@
+package lombok.data;
+
+import lombok.Data;
+
+/**
+ * Created by hermes on 2017/8/31.
+ */
+@Data
+public class Book {
+    private int id;
+    private String title;
+    private String name;
+    private String date;
+}
