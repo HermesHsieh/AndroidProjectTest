d887e603da5b408b52a1d9bc8504779f21e516ba
Hermes <hermes.hsieh@git4u.com>
2017-09-22 16:46:55 +0800

    Add Custom FooterBehavior.

---
+++ app/src/main/java/tw/android/test/activity/collapsing/FooterBehavior.java
@@ -0,0 +1,115 @@
+package tw.android.test.activity.collapsing;
+
+import android.content.Context;
+import android.os.Build;
+import android.support.design.widget.CoordinatorLayout;
+import android.support.v13.view.ViewCompat;
+import android.support.v4.view.ViewPropertyAnimatorListener;
+import android.support.v4.view.animation.FastOutSlowInInterpolator;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.animation.Interpolator;
+
+/**
+ * Created by hermes.hsieh on 2017/9/22.
+ */
+
+public class FooterBehavior extends CoordinatorLayout.Behavior<View> {
+
+    private static final Interpolator INTERPOLATOR = new FastOutSlowInInterpolator();
+
+    private boolean mIsAnimatingOut = false;
+
+    private int sinceDirectionChange;
+
+    public FooterBehavior(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    @Override
+    public boolean onStartNestedScroll(final CoordinatorLayout coordinatorLayout, final View child,
+                                       final View directTargetChild, final View target, final int nestedScrollAxes) {
+        // Ensure we react to vertical scrolling
+        Log.d("ttttt", "onStartNestedScroll");
+        return nestedScrollAxes == ViewCompat.SCROLL_AXIS_VERTICAL
+                || super.onStartNestedScroll(coordinatorLayout, child, directTargetChild, target, nestedScrollAxes);
+    }
+
+    @Override
+    public void onNestedScroll(CoordinatorLayout coordinatorLayout, View child, View target, int dxConsumed, int dyConsumed, int dxUnconsumed, int dyUnconsumed) {
+        super.onNestedScroll(coordinatorLayout, child, target, dxConsumed, dyConsumed, dxUnconsumed, dyUnconsumed);
+        Log.d("ttttt", "onNestedScroll");
+        Log.d("ttttt", "dxConsumed : " + dxConsumed + ", dyConsumed : " + dyConsumed + ", dxUnconsumed : " + dxUnconsumed + ", dyUnconsumed : " + dyUnconsumed);
+        Log.d("ttttt", "child visible : " + child.getVisibility());
+        if (dyConsumed > 0 && !this.mIsAnimatingOut && child.getVisibility() == View.VISIBLE) {
+            // User scrolled down and the FAB is currently visible -> hide the FAB
+            animateOut(child);
+        } else if (dyConsumed < 0 && child.getVisibility() != View.VISIBLE) {
+            // User scrolled up and the FAB is currently not visible -> show the FAB
+            animateIn(child);
+        }
+    }
+
+//    @Override
+//    public boolean layoutDependsOn(CoordinatorLayout parent, View child, View dependency) {
+//        Log.d("ttttt", "layoutDependsOn");
+//        return dependency instanceof AppBarLayout;
+//    }
+//
+//    @Override
+//    public boolean onDependentViewChanged(CoordinatorLayout parent, View child, View dependency) {
+//        Log.d("ttttt", "onDependentViewChanged");
+//        float translationY = Math.abs(dependency.getTranslationY());
+//        child.setTranslationY(translationY);
+//        return true;
+//    }
+
+    // Same animation that FloatingActionButton.Behavior uses to hide the FAB when the AppBarLayout exits
+    private void animateOut(final View view) {
+        if (Build.VERSION.SDK_INT >= 14) {
+            ViewCompat.animate(view).translationY(view.getHeight() + getMarginBottom(view)).setInterpolator(INTERPOLATOR).withLayer()
+                    .setListener(new ViewPropertyAnimatorListener() {
+                        public void onAnimationStart(View view) {
+                            Log.d("ttttt", "onAnimationStart");
+                            FooterBehavior.this.mIsAnimatingOut = true;
+                        }
+
+                        public void onAnimationCancel(View view) {
+                            Log.d("ttttt", "onAnimationCancel");
+                            FooterBehavior.this.mIsAnimatingOut = false;
+                        }
+
+                        public void onAnimationEnd(View view) {
+                            Log.d("ttttt", "onAnimationEnd");
+                            FooterBehavior.this.mIsAnimatingOut = false;
+                            view.setVisibility(View.INVISIBLE);
+                        }
+                    }).start();
+        } else {
+
+        }
+    }
+
+    // Same animation that FloatingActionButton.Behavior uses to show the FAB when the AppBarLayout enters
+    private void animateIn(View view) {
+        view.setVisibility(View.VISIBLE);
+        if (Build.VERSION.SDK_INT >= 14) {
+            ViewCompat.animate(view).translationY(0)
+                    .setInterpolator(INTERPOLATOR).withLayer().setListener(null)
+                    .start();
+        } else {
+
+        }
+    }
+
+    private int getMarginBottom(View v) {
+        int marginBottom = 0;
+        final ViewGroup.LayoutParams layoutParams = v.getLayoutParams();
+        if (layoutParams instanceof ViewGroup.MarginLayoutParams) {
+            marginBottom = ((ViewGroup.MarginLayoutParams) layoutParams).bottomMargin;
+        }
+        return marginBottom;
+    }
+}
