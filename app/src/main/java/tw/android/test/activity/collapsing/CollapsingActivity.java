caa9d9b953f3a9e04ded5d6d511720fbc9a96fb3
Hermes <hermes.hsieh@git4u.com>
2017-09-28 15:58:16 +0800

    Test FLAG_ACTIVITY_REORDER_TO_FRONT flags.

--- app/src/main/java/tw/android/test/activity/collapsing/CollapsingActivity.java
+++ app/src/main/java/tw/android/test/activity/collapsing/CollapsingActivity.java
@@ -2,10 +2,13 @@ package tw.android.test.activity.collapsing;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.design.widget.AppBarLayout;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 
 import com.example.hermes.test.R;
 
@@ -59,9 +62,55 @@ public class CollapsingActivity extends BaseSimpleActivity {
         mAdapter.notifyDataSetChanged();
     }
 
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Log.d("Collapsing", "onCreate--->");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d("Collapsing", "onStart--->");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d("Collapsing", "onResume--->");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d("Collapsing", "onPause--->");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d("Collapsing", "onStop--->");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d("Collapsing", "onDestroy--->");
+    }
+
+    @Override
+    protected void onNewIntent(Intent intent) {
+        super.onNewIntent(intent);
+        Log.d("Collapsing", "onNewIntent--->");
+        mRecyclerView.scrollToPosition(0);
+        mAdapter.onCreateData(30);
+        mAdapter.notifyDataSetChanged();
+    }
+
     public static void launch(Activity activity) {
         Intent intent = new Intent(activity, CollapsingActivity.class);
         intent.setFlags(Intent.FLAG_ACTIVITY_REORDER_TO_FRONT);
+//        intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_SINGLE_TOP);
         activity.startActivity(intent);
     }
 
