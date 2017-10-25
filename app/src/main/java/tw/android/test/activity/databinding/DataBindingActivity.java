40da848f403d65ceb29e9142cad1517e1ced1026
Hermes <hermes@xieyixiande-MacBook-Pro.local>
2017-10-25 23:07:45 +0800

    Add BindData with click event.

--- app/src/main/java/tw/android/test/activity/databinding/DataBindingActivity.java
+++ app/src/main/java/tw/android/test/activity/databinding/DataBindingActivity.java
@@ -3,6 +3,8 @@ package tw.android.test.activity.databinding;
 import android.app.Activity;
 import android.content.Intent;
 import android.databinding.DataBindingUtil;
+import android.util.Log;
+import android.widget.Toast;
 
 import com.example.hermes.test.R;
 import com.example.hermes.test.databinding.ActivityDataBindBinding;
@@ -16,16 +18,25 @@ import tw.android.test.data.User;
 
 public class DataBindingActivity extends BaseSimpleActivity {
 
+    private ActivityDataBindBinding mMBinding;
+
     public static void launch(Activity activity) {
         Intent intent = new Intent(activity, DataBindingActivity.class);
         activity.startActivity(intent);
     }
 
+    public void onClickShowToastButton(User user) {
+        Log.d("testtt", "click");
+        Toast.makeText(this, "Click button! " + user.firstName + " " + user.lastName, Toast.LENGTH_SHORT).show();
+        mMBinding.setUser(new User("Hahaha", "YOyoyoyoyo"));
+    }
+
     @Override
     protected void initView() {
-        ActivityDataBindBinding binding = DataBindingUtil.setContentView(this, R.layout.activity_data_bind);
+        mMBinding = DataBindingUtil.setContentView(this, R.layout.activity_data_bind);
+        mMBinding.setHandlers(this);
         User user = new User("Test", "User");
-        binding.setUser(user);
+        mMBinding.setUser(user);
     }
 
     @Override
