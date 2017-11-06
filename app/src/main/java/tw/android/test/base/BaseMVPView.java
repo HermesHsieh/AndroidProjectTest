a236af64f9276fe13b3d85fd5ddb2cf60e0529cb
Hermes <hermes.hsieh@git4u.com>
2017-11-06 16:20:24 +0800

    Update BaseMVPView.

--- app/src/main/java/tw/android/test/base/BaseMVPView.java
+++ app/src/main/java/tw/android/test/base/BaseMVPView.java
@@ -4,7 +4,7 @@ package tw.android.test.base;
  * Created by hermes.hsieh on 2017/11/6.
  */
 
-public interface BaseMVPView<T extends BasePresenter> {
+public interface BaseMVPView<T extends BasePresenter> extends BaseView {
 
     void setPresenter(T presenter);
 }
