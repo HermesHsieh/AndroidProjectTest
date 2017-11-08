7902212e6f0327fd20d5bf3ca796c213590daf10
Hermes <hermes.hsieh@git4u.com>
2017-11-08 09:48:32 +0800

    Testing verify string.

--- app/src/test/java/com/example/hermes/test/activity/login/LoginPresenterTest.java
+++ app/src/test/java/com/example/hermes/test/activity/login/LoginPresenterTest.java
@@ -1,5 +1,8 @@
 package com.example.hermes.test.activity.login;
 
+import android.content.Context;
+import android.content.pm.ApplicationInfo;
+
 import com.afollestad.materialdialogs.MaterialDialog;
 import com.example.hermes.test.R;
 
@@ -15,7 +18,11 @@ import tw.android.test.activity.login.LoginActivity;
 import tw.android.test.activity.login.LoginPresenter;
 
 import static com.example.hermes.test.R.string.account;
+import static junit.framework.TestCase.assertEquals;
+import static org.mockito.Matchers.anyString;
+import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 /**
  * Created by hermes.hsieh on 2017/11/6.
@@ -25,6 +32,12 @@ public class LoginPresenterTest {
 
     @Mock
     private LoginActivity mLoginView;
+
+    @Mock
+    Context mMockContext;
+
+    @Mock
+    ApplicationInfo mApplicationInfo;
 
     @Mock
     private MaterialDialog.SingleButtonCallback mSingleButtonCallback;
@@ -43,6 +56,7 @@ public class LoginPresenterTest {
 
 //        // The presenter wont't update the view unless it's active.
 //        when(mLoginView.isActive()).thenReturn(true);
+//        when(mMockContext.getApplicationInfo()).thenReturn(mApplicationInfo);
     }
 
     @Test
@@ -59,7 +73,16 @@ public class LoginPresenterTest {
         mLoginPresenter = new LoginPresenter(mLoginView);
 
         mLoginPresenter.onClickLoginButton("", TEST_PASSWORD);
-        verify(mLoginView).showMsgDialog(mLoginView.getString(R.string.e_input_empty, mLoginView.getString(account)));
+
+//        doReturn("請輸入帳號")
+                when(mMockContext.getString(R.string.e_input_empty, mMockContext.getString(R.string.account)))
+                .thenReturn("請輸入帳號");
+
+        String result = mMockContext.getString(R.string.e_input_empty, mMockContext.getString(R.string.account));
+        assertEquals(result, "請輸入帳號");
+
+//        verify(mLoginView).showMsgDialog(result);
+//        assertEquals("請輸入帳號", mMockContext.getString(R.string.e_input_empty, mMockContext.getString(account)));
 //        verify(mLoginView).showMsgDialog("請輸入帳號");
     }
 
@@ -68,7 +91,7 @@ public class LoginPresenterTest {
         mLoginPresenter = new LoginPresenter(mLoginView);
 
         mLoginPresenter.onClickLoginButton(null, TEST_PASSWORD);
-        verify(mLoginView).showMsgDialog(mLoginView.getString(R.string.e_input_empty, mLoginView.getString(account)));
+        verify(mLoginView).showMsgDialog(mLoginView.getString(R.string.e_input_empty, mLoginView.getString(R.string.account)));
     }
 
     @Test
@@ -84,7 +107,24 @@ public class LoginPresenterTest {
         mLoginPresenter = new LoginPresenter(mLoginView);
 
         mLoginPresenter.onClickLoginButton(TEST_ACCOUNT, null);
-        verify(mLoginView).showMsgDialog(mLoginView.getString(R.string.e_input_empty, mLoginView.getString(R.string.password)));
+
+//        when(mMockContext.getString(R.string.e_input_empty))
+//                .thenReturn("請輸入%s");
+//        when(mMockContext.getString(R.string.password))
+//                .thenReturn("密碼");
+
+        verify(mLoginView).showMsgDialog(mMockContext.getString(R.string.e_input_empty, mMockContext.getString(R.string.password)));
+    }
+
+    @Test
+    public void userOnClickLogin_Input_All_Empty_showsErrorUi() {
+        mLoginPresenter = new LoginPresenter(mLoginView);
+
+        mLoginPresenter.onClickLoginButton("", "");
+        verify(mLoginView).showMsgDialog(mLoginView.getString(R.string.e_input_empty, mLoginView.getString(account)));
+        verify(mLoginView, never()).showMsgDialog(mLoginView.getString(R.string.e_input_empty, mLoginView.getString(R.string.password)));
+        verify(mLoginView, never()).setStateText(anyString());
+        verify(mLoginView, never()).setAccountInputEnable(false);
     }
 
     @Test
