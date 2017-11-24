76d5908e03a34cf6f896c4f37494e3e40fcac03a
Hermes <hermes.hsieh@git4u.com>
2017-11-24 15:52:39 +0800

    Add test OffsetDateTime.

--- app/src/main/java/tw/android/test/activity/pickdate/PickDateActivity.java
+++ app/src/main/java/tw/android/test/activity/pickdate/PickDateActivity.java
@@ -5,12 +5,19 @@ import android.app.DatePickerDialog;
 import android.content.Intent;
 import android.support.constraint.Guideline;
 import android.support.design.widget.TextInputLayout;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.TextView;
 
 import com.example.hermes.test.R;
+
+import org.threeten.bp.OffsetDateTime;
+import org.threeten.bp.ZoneOffset;
+import org.threeten.bp.format.DateTimeFormatter;
+
+import java.util.Locale;
 
 import butterknife.BindView;
 import butterknife.OnClick;
@@ -21,6 +28,8 @@ import tw.android.test.base.BaseSimpleActivity;
  */
 
 public class PickDateActivity extends BaseSimpleActivity implements PickDateContract.View {
+
+    private final static String TAG = PickDateActivity.class.getSimpleName();
 
     @BindView(R.id.guide_line)
     Guideline guideLine;
@@ -56,6 +65,47 @@ public class PickDateActivity extends BaseSimpleActivity implements PickDateCont
     protected void initData() {
         startDate.setText("請選擇開始日期");
         endDate.setText("請選擇結束日期");
+        testOffSetDateTime();
+    }
+
+    private void testOffSetDateTime() {
+        // 2017-11-16T12:28:48.827Z
+        OffsetDateTime offsetDateTime1 = OffsetDateTime.of(2017, 11, 16, 12, 28, 48, 0, ZoneOffset.ofHours(0));
+        Log.d(TAG, "OffsetDateTime1 : " + offsetDateTime1);
+
+        OffsetDateTime offsetDateTime2 = OffsetDateTime.of(2017, 11, 16, 12, 28, 48, 0, ZoneOffset.ofHours(8));
+        Log.d(TAG, "OffsetDateTime2 : " + offsetDateTime2);
+
+        OffsetDateTime offsetDateTime3 = OffsetDateTime.of(2017, 11, 16, 12, 28, 48, 0, ZoneOffset.of("+08:00"));
+        Log.d(TAG, "OffsetDateTime3 : " + offsetDateTime3);
+
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss.SSS", Locale.TAIWAN);
+        DateTimeFormatter formatter2 = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'", Locale.JAPAN);
+        DateTimeFormatter formatter3 = DateTimeFormatter.ofPattern("MM/dd/yyyy hh:mm:ss a", Locale.US);
+
+        Log.d(TAG, "Time1 Formatter1 : " + getAppDateTimeNormal(offsetDateTime1));
+
+//        Log.d(TAG, "Time1 Formatter2 : " + offsetDateTime1.format(formatter2));
+//        Log.d(TAG, "Time1 Formatter3 : " + offsetDateTime1.format(formatter3));
+//        Log.d(TAG, "Time3 Formatter1 : " + offsetDateTime3.format(formatter));
+//        Log.d(TAG, "Time3 Formatter2 : " + offsetDateTime3.format(formatter2));
+//        Log.d(TAG, "Time3 Formatter3 : " + offsetDateTime3.format(formatter3));
+
+        startDate.setText(offsetDateTime3.format(formatter3));
+    }
+
+    private static DateTimeFormatter DATE_TIME_LAST_SECS = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss", Locale.TAIWAN);
+    private static DateTimeFormatter DATE_TIME_LAST_NANO_SECS = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss.SSS", Locale.TAIWAN);
+    private static DateTimeFormatter DATE_TIME_LAST_MINS = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm", Locale.TAIWAN);
+    private static DateTimeFormatter DATE_TIME_LAST_DAYS = DateTimeFormatter.ofPattern("yyyy-MM-dd", Locale.TAIWAN);
+    private static DateTimeFormatter DATE_TIME_LAST_SECS_WITH_AM_PM = DateTimeFormatter.ofPattern("MM/dd/yyyy hh:mm:ss a", Locale.US);
+
+    public static String getAppDateTimeNormal(OffsetDateTime offsetDateTime) {
+        return getDateTimeFormat(DATE_TIME_LAST_SECS, offsetDateTime);
+    }
+
+    private static String getDateTimeFormat(DateTimeFormatter dateTimeFormatter, OffsetDateTime offsetDateTime) {
+        return offsetDateTime.format(dateTimeFormatter);
     }
 
     @OnClick({R.id.start_date, R.id.end_date, R.id.pick})
