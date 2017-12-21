40197ae10a9248b8a335d3d5ed3dc970dfd91b15
Hermes <hermes.hsieh@git4u.com>
2017-12-21 14:25:03 +0800

    Add annotations ptah class.

---
+++ app/src/main/java/tw/android/test/annotations/Verify.java
@@ -0,0 +1,11 @@
+package tw.android.test.annotations;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+/**
+ * Created by hermes.hsieh on 2017/12/21.
+ */
+@Retention(RetentionPolicy.RUNTIME)
+public @interface Verify {
+}
