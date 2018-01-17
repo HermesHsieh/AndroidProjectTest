71efbc5cf6f4c7a51986b5c61bb4e1548ed04ea7
Hermes <hermes.hsieh@git4u.com>
2018-01-17 12:21:24 +0800

    Update pick date test format.

--- app/src/main/java/tw/android/test/activity/pickdate/PickDateActivity.java
+++ app/src/main/java/tw/android/test/activity/pickdate/PickDateActivity.java
@@ -14,7 +14,9 @@ import android.widget.TextView;
 import com.example.hermes.test.R;
 
 import org.threeten.bp.OffsetDateTime;
+import org.threeten.bp.ZoneId;
 import org.threeten.bp.ZoneOffset;
+import org.threeten.bp.ZonedDateTime;
 import org.threeten.bp.format.DateTimeFormatter;
 
 import java.util.Locale;
@@ -71,19 +73,19 @@ public class PickDateActivity extends BaseSimpleActivity implements PickDateCont
     private void testOffSetDateTime() {
         // 2017-11-16T12:28:48.827Z
         OffsetDateTime offsetDateTime1 = OffsetDateTime.of(2017, 11, 16, 12, 28, 48, 0, ZoneOffset.ofHours(0));
-        Log.d(TAG, "OffsetDateTime1 : " + offsetDateTime1);
+//        Log.d(TAG, "OffsetDateTime1 : " + offsetDateTime1);
 
         OffsetDateTime offsetDateTime2 = OffsetDateTime.of(2017, 11, 16, 12, 28, 48, 0, ZoneOffset.ofHours(8));
-        Log.d(TAG, "OffsetDateTime2 : " + offsetDateTime2);
+//        Log.d(TAG, "OffsetDateTime2 : " + offsetDateTime2);
 
         OffsetDateTime offsetDateTime3 = OffsetDateTime.of(2017, 11, 16, 12, 28, 48, 0, ZoneOffset.of("+08:00"));
-        Log.d(TAG, "OffsetDateTime3 : " + offsetDateTime3);
+//        Log.d(TAG, "OffsetDateTime3 : " + offsetDateTime3);
 
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss.SSS", Locale.TAIWAN);
         DateTimeFormatter formatter2 = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'", Locale.JAPAN);
         DateTimeFormatter formatter3 = DateTimeFormatter.ofPattern("MM/dd/yyyy hh:mm:ss a", Locale.US);
 
-        Log.d(TAG, "Time1 Formatter1 : " + getAppDateTimeNormal(offsetDateTime1));
+//        Log.d(TAG, "Time1 Formatter1 : " + getAppDateTimeNormal(offsetDateTime1));
 
 //        Log.d(TAG, "Time1 Formatter2 : " + offsetDateTime1.format(formatter2));
 //        Log.d(TAG, "Time1 Formatter3 : " + offsetDateTime1.format(formatter3));
@@ -92,6 +94,49 @@ public class PickDateActivity extends BaseSimpleActivity implements PickDateCont
 //        Log.d(TAG, "Time3 Formatter3 : " + offsetDateTime3.format(formatter3));
 
         startDate.setText(offsetDateTime3.format(formatter3));
+        OffsetDateTime offsetDateTime = null;
+        try {
+            offsetDateTime = OffsetDateTime.parse("2018-01-10T11:39:30.630Z", DateTimeFormatter.ISO_OFFSET_DATE_TIME);
+            Log.d(TAG, "OffsetDateTime.parse : " + offsetDateTime);
+        } catch (Exception e) {
+            e.printStackTrace();
+            Log.e(TAG, "OffsetDateTime.parse : " + e.getMessage());
+        }
+
+        OffsetDateTime offsetDateTime20 = null;
+        try {
+            offsetDateTime20 = getOffsetDateTime("2018-01-10T10:39:30.630Z");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        try {
+            offsetDateTime20 = OffsetDateTime.parse("2018-01-10T10:39:30.630Z", DateTimeFormatter.ISO_OFFSET_DATE_TIME);
+            Log.d(TAG, "OffsetDateTime.parse : " + offsetDateTime20);
+            Log.d(TAG, "OffsetDateTime compareTo20 : " + offsetDateTime.compareTo(offsetDateTime20));
+        } catch (Exception e) {
+            e.printStackTrace();
+            Log.e(TAG, "OffsetDateTime.parse : " + e.getMessage());
+        }
+
+
+        try {
+            ZonedDateTime zonedDateTime = ZonedDateTime.parse("2018-01-10T11:39:30.630", DateTimeFormatter.ISO_LOCAL_DATE_TIME.withZone(ZoneId.systemDefault()));
+            Log.d(TAG, "ZonedDateTime.parse : " + zonedDateTime);
+        } catch (Exception e) {
+            e.printStackTrace();
+            Log.e(TAG, "ZonedDateTime.parse : " + e.getMessage());
+        }
+    }
+
+    public static OffsetDateTime getOffsetDateTime(String dateTime) {
+        OffsetDateTime offsetDateTime = null;
+        try {
+            offsetDateTime = OffsetDateTime.parse(dateTime, DateTimeFormatter.ISO_OFFSET_DATE_TIME);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return offsetDateTime;
     }
 
     private static DateTimeFormatter DATE_TIME_LAST_SECS = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss", Locale.TAIWAN);
