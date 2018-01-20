3eff2f1facbd035862d3c1aaf44e38ca78e60ec8
Hermes <hermes.hsieh@git4u.com>
2018-01-20 11:57:28 +0800

    Add userNameItem in InputTextLayoutActivity.

--- app/src/main/java/tw/android/test/activity/inputtextlayout/InputTextLayoutActivity.java
+++ app/src/main/java/tw/android/test/activity/inputtextlayout/InputTextLayoutActivity.java
@@ -2,16 +2,29 @@ package tw.android.test.activity.inputtextlayout;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.widget.Button;
 
 import com.example.hermes.test.R;
 
+import butterknife.BindView;
 import tw.android.test.base.BaseSimpleActivity;
+import tw.android.test.ui.form.EditTextItem;
+import tw.android.test.ui.form.FormView;
 
 /**
  * Created by hermes.hsieh on 2018/1/20.
  */
 
 public class InputTextLayoutActivity extends BaseSimpleActivity {
+
+    @BindView(R.id.form_view)
+    FormView formView;
+    @BindView(R.id.btn)
+    Button nextStep;
+
+    FormView.Adapter adapter;
+
+    EditTextItem userNameItem;
 
     public static void launch(Activity activity) {
         Intent intent = new Intent(activity, InputTextLayoutActivity.class);
@@ -26,6 +39,12 @@ public class InputTextLayoutActivity extends BaseSimpleActivity {
     @Override
     protected void initView() {
         setTitle(this.getClass().getSimpleName());
+
+        adapter = new FormView.Adapter();
+
+        userNameItem = new EditTextItem(this, getString(R.string.register_p_account));
+        adapter.add(userNameItem);
+        formView.setAdapter(adapter);
     }
 
     @Override
