7f3e49cc0e2f2a058ced172445e3d83af85da61b
Hermes <hermes.hsieh@git4u.com>
2018-01-21 12:27:04 +0800

    Update EditTextActionImageItem.

--- app/src/main/java/tw/android/test/ui/form/EditTextActionImageItem.java
+++ app/src/main/java/tw/android/test/ui/form/EditTextActionImageItem.java
@@ -4,10 +4,6 @@ import android.content.Context;
 import android.support.annotation.DrawableRes;
 import android.support.annotation.NonNull;
 import android.support.v4.content.ContextCompat;
-import android.text.Editable;
-import android.text.TextUtils;
-import android.text.TextWatcher;
-import android.view.View;
 import android.widget.ImageView;
 import android.widget.ProgressBar;
 
@@ -23,23 +19,6 @@ public class EditTextActionImageItem extends EditTextItem {
 
     @BindView(R.id.action_image)
     ImageView mActionImage;
-
-    public enum ACTION_STATE {
-        NONE(0),
-        LOADING(1),
-        CORRECT(1),
-        ERROR(2);
-
-        private int value;
-
-        ACTION_STATE(int value) {
-            this.value = value;
-        }
-
-        public Integer getValue() {
-            return value;
-        }
-    }
 
     @Override
     public int getLayoutId() {
@@ -66,66 +45,12 @@ public class EditTextActionImageItem extends EditTextItem {
         editText.setCompoundDrawablesWithIntrinsicBounds(null, null, ContextCompat.getDrawable(context, rightIcon), null);
     }
 
-    private boolean isEnableActionListener;
-
-    public void setActionListenerToggleEnabled(boolean enable, final OnActionStateListener onActionStateListener) {
-        if (editText == null) {
-            return;
-        }
-
-        isEnableActionListener = enable;
-        if (enable && onActionStateListener != null) {
-            editText.addTextChangedListener(new TextWatcher() {
-                @Override
-                public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-
-                }
-
-                @Override
-                public void onTextChanged(CharSequence s, int start, int before, int count) {
-
-                }
-
-                @Override
-                public void afterTextChanged(Editable s) {
-                    if (TextUtils.isEmpty(s.toString())) {
-                        onActionStateListener.afterTextEmpty();
-                    } else {
-                        onActionStateListener.afterTextChanged(s);
-                    }
-                }
-            });
-        } else {
-            editText.addTextChangedListener(null);
-        }
+    public ProgressBar getProgressBar() {
+        return mProgressBar;
     }
 
-    public interface OnActionStateListener {
-        void afterTextEmpty();
-
-        void afterTextChanged(Editable s);
-    }
-
-    public void onActionStateVisibility(ACTION_STATE state) {
-        switch (state) {
-            default:
-            case NONE:
-                mProgressBar.setVisibility(View.GONE);
-                mActionImage.setVisibility(View.GONE);
-                break;
-            case LOADING:
-                mProgressBar.setVisibility(View.VISIBLE);
-                mActionImage.setVisibility(View.GONE);
-                break;
-            case CORRECT:
-                mProgressBar.setVisibility(View.GONE);
-                mActionImage.setVisibility(View.VISIBLE);
-                break;
-            case ERROR:
-                mProgressBar.setVisibility(View.GONE);
-                mActionImage.setVisibility(View.VISIBLE);
-                break;
-        }
+    public ImageView getActionImage() {
+        return mActionImage;
     }
 
     public void setActionImageDrawable(int drawable) {
