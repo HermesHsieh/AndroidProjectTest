5f8142527389ce6786bbf0f1c8d8bc54ae13b886
Hermes <hermes.hsieh@git4u.com>
2018-03-14 17:19:16 +0800

    Add theme with widget for test.

--- app/src/main/java/tw/android/test/activity/theme/ThemeActivity.java
+++ app/src/main/java/tw/android/test/activity/theme/ThemeActivity.java
@@ -3,9 +3,15 @@ package tw.android.test.activity.theme;
 import android.app.Activity;
 import android.content.Intent;
 import android.support.design.widget.TabLayout;
+import android.support.design.widget.TextInputLayout;
+import android.support.v7.widget.AppCompatSpinner;
+import android.widget.ArrayAdapter;
 
 import com.afollestad.materialdialogs.MaterialDialog;
 import com.example.hermes.test.R;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import butterknife.BindView;
 import butterknife.OnClick;
@@ -16,6 +22,12 @@ public class ThemeActivity extends BaseSimpleActivity {
 
     @BindView(R.id.tabs)
     TabLayout tabLayout;
+
+    @BindView(R.id.input_layout_2)
+    TextInputLayout textInputLayout2;
+
+    @BindView(R.id.spinner)
+    AppCompatSpinner spinner;
 
     public static void launch(Activity activity) {
         Intent intent = new Intent(activity, ThemeActivity.class);
@@ -32,6 +44,21 @@ public class ThemeActivity extends BaseSimpleActivity {
         tabLayout.addTab(tabLayout.newTab().setText("Tab1"));
         tabLayout.addTab(tabLayout.newTab().setText("Tab2"));
         tabLayout.addTab(tabLayout.newTab().setText("Tab3"));
+
+        textInputLayout2.setError("Error text!");
+
+        List<String> list = new ArrayList<>();
+        for (int i = 0; i < 5; i++) {
+            list.add("Item[" + i + "]");
+        }
+
+        ArrayAdapter spinnerAdapter = new ArrayAdapter<>(
+                this,
+                android.R.layout.simple_spinner_dropdown_item,
+                list
+        );
+
+        spinner.setAdapter(spinnerAdapter);
     }
 
     @Override
