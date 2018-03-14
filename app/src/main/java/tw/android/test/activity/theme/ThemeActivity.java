538adaf2363e1e856a076d18b7e5e0c9870609a8
Hermes <hermes.hsieh@git4u.com>
2018-03-14 14:51:32 +0800

    Add layout content in activity_theme.xml.

--- app/src/main/java/tw/android/test/activity/theme/ThemeActivity.java
+++ app/src/main/java/tw/android/test/activity/theme/ThemeActivity.java
@@ -2,13 +2,20 @@ package tw.android.test.activity.theme;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.support.design.widget.TabLayout;
 
+import com.afollestad.materialdialogs.MaterialDialog;
 import com.example.hermes.test.R;
 
+import butterknife.BindView;
+import butterknife.OnClick;
 import tw.android.test.base.BaseSimpleActivity;
 
 
 public class ThemeActivity extends BaseSimpleActivity {
+
+    @BindView(R.id.tabs)
+    TabLayout tabLayout;
 
     public static void launch(Activity activity) {
         Intent intent = new Intent(activity, ThemeActivity.class);
@@ -22,11 +29,23 @@ public class ThemeActivity extends BaseSimpleActivity {
 
     @Override
     protected void initView() {
-
+        tabLayout.addTab(tabLayout.newTab().setText("Tab1"));
+        tabLayout.addTab(tabLayout.newTab().setText("Tab2"));
+        tabLayout.addTab(tabLayout.newTab().setText("Tab3"));
     }
 
     @Override
     protected void initData() {
 
+    }
+
+    @OnClick(R.id.show_dialog_button)
+    public void onClickShowDialog() {
+        new MaterialDialog.Builder(this)
+                .title("Title")
+                .content("content")
+                .positiveText("OK")
+                .negativeText("Cancel")
+                .show();
     }
 }
