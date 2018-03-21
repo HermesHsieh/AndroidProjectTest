7c126541db1bf55854552be6a274d048d20c6815
Hermes <hermes.hsieh@git4u.com>
2018-03-21 09:18:20 +0800

    Add test video in VideoActivity.

--- app/src/main/java/tw/android/test/activity/video/VideoActivity.java
+++ app/src/main/java/tw/android/test/activity/video/VideoActivity.java
@@ -2,10 +2,23 @@ package tw.android.test.activity.video;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.media.MediaPlayer;
+import android.util.Log;
+import android.widget.MediaController;
+import android.widget.VideoView;
 
+import com.example.hermes.test.R;
+
+import butterknife.BindView;
 import tw.android.test.base.BaseSimpleActivity;
 
 public class VideoActivity extends BaseSimpleActivity {
+
+    @BindView(R.id.videoView)
+    VideoView mVideoView;
+
+    int position = 0;
+    MediaController mMediaController;
 
     public static void launch(Activity activity) {
         Intent intent = new Intent(activity, VideoActivity.class);
@@ -14,16 +27,49 @@ public class VideoActivity extends BaseSimpleActivity {
 
     @Override
     protected void setContentView() {
-
+        setContentView(R.layout.activity_video);
     }
 
     @Override
     protected void initView() {
-
     }
 
     @Override
     protected void initData() {
+        if (mMediaController == null) {
+            mMediaController = new MediaController(this);
 
+            mVideoView.setMediaController(mMediaController);
+        }
+
+        try {
+            // video file.
+            mVideoView.setVideoPath("http://techslides.com/demos/sample-videos/small.mp4");
+        } catch (Exception e) {
+            Log.e("Error", e.getMessage());
+            e.printStackTrace();
+        }
+
+        mVideoView.requestFocus();
+
+        // When the video file ready for playback.
+        mVideoView.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
+            public void onPrepared(MediaPlayer mediaPlayer) {
+                mVideoView.seekTo(position);
+                if (position == 0) {
+                    mVideoView.start();
+                }
+
+                // When video Screen change size.
+                mediaPlayer.setOnVideoSizeChangedListener(new MediaPlayer.OnVideoSizeChangedListener() {
+                    @Override
+                    public void onVideoSizeChanged(MediaPlayer mp, int width, int height) {
+
+                        // Re-Set the mVideoView that acts as the anchor for the MediaController
+                        mMediaController.setAnchorView(mVideoView);
+                    }
+                });
+            }
+        });
     }
 }
