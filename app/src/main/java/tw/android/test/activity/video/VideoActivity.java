485ea7196be5d0df4f5bb7ca38f76773d3b35855
Hermes <hermes.hsieh@git4u.com>
2018-03-27 14:18:57 +0800

    Update VideoActivity.

--- app/src/main/java/tw/android/test/activity/video/VideoActivity.java
+++ app/src/main/java/tw/android/test/activity/video/VideoActivity.java
@@ -4,7 +4,11 @@ import android.app.Activity;
 import android.content.Intent;
 import android.content.res.Configuration;
 import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.util.Log;
+import android.view.View;
+import android.view.WindowManager;
 import android.widget.MediaController;
 import android.widget.VideoView;
 
@@ -23,6 +27,41 @@ public class VideoActivity extends BaseSimpleActivity {
     int position = 0;
     MediaController mMediaController;
 
+    // http://techslides.com/demos/sample-videos/small.mp4
+    String videoUrl = "http://clips.vorwaerts-gmbh.de/VfE_html5.mp4";
+    String videoUrl2 = "rtsp://184.72.239.149/vod/mp4:BigBuckBunny_175k.mov";
+    String videoUrl3 = "http://www.wowza.com/_h264/BigBuckBunny_175k.mov";
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        Log.i(TAG, "onCreate");
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    protected void onResume() {
+        Log.i(TAG, "onResume");
+        super.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        Log.i(TAG, "onPause");
+        super.onPause();
+    }
+
+    @Override
+    protected void onStop() {
+        Log.i(TAG, "onStop");
+        super.onStop();
+    }
+
+    @Override
+    protected void onDestroy() {
+        Log.i(TAG, "onDestroy");
+        super.onDestroy();
+    }
+
     public static void launch(Activity activity) {
         Intent intent = new Intent(activity, VideoActivity.class);
         activity.startActivity(intent);
@@ -30,15 +69,18 @@ public class VideoActivity extends BaseSimpleActivity {
 
     @Override
     protected void setContentView() {
+        Log.d(TAG, "setContentView");
         setContentView(R.layout.activity_video);
     }
 
     @Override
     protected void initView() {
+        Log.d(TAG, "initView");
     }
 
     @Override
     protected void initData() {
+        Log.d(TAG, "initData");
         if (mMediaController == null) {
             mMediaController = new MediaController(this);
 
@@ -47,7 +89,7 @@ public class VideoActivity extends BaseSimpleActivity {
 
         try {
             // video file.
-            mVideoView.setVideoPath("http://techslides.com/demos/sample-videos/small.mp4");
+            mVideoView.setVideoPath(videoUrl2);
         } catch (Exception e) {
             Log.e("Error", e.getMessage());
             e.printStackTrace();
@@ -79,14 +121,46 @@ public class VideoActivity extends BaseSimpleActivity {
     @Override
     public void onConfigurationChanged(Configuration newConfig) {
         super.onConfigurationChanged(newConfig);
-        Log.v(TAG, "onConfigurationChanged");
+        Log.i(TAG, "onConfigurationChanged");
         setContentView();
+        initView();
+        initData();
         if (newConfig.orientation == Configuration.ORIENTATION_LANDSCAPE) {
             Log.d(TAG, "ORIENTATION_LANDSCAPE");
+            getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
+            if (getSupportActionBar() != null) {
+                getSupportActionBar().isShowing();
+                getSupportActionBar().hide();
+            }
+
+            View decorView = getWindow().getDecorView();
+            // Hide both the navigation bar and the status bar.
+            // SYSTEM_UI_FLAG_FULLSCREEN is only available on Android 4.1 and higher, but as
+            // a general rule, you should design your app to hide the status bar whenever you
+            // hide the navigation bar.
+            int uiOptions = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                    | View.SYSTEM_UI_FLAG_FULLSCREEN;
+            decorView.setSystemUiVisibility(uiOptions);
         } else if (newConfig.orientation == Configuration.ORIENTATION_PORTRAIT) {
             Log.d(TAG, "ORIENTATION_PORTRAIT");
+            getWindow().clearFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
+            if (getSupportActionBar() != null) {
+                getSupportActionBar().show();
+            }
         } else {
             Log.d(TAG, "ORIENTATION_UNDEFINED");
         }
+    }
+
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        Log.i(TAG, "onRestoreInstanceState");
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        Log.i(TAG, "onSaveInstanceState");
     }
 }
