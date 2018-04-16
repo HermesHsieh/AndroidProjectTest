319f3108c7081e7b95c8578391d1d2e67e051a7a
Hermes <hermes.hsieh@git4u.com>
2018-04-16 15:54:38 +0800

    Create lib_base module.

---
+++ lib_base/src/main/java/com/git4u/interview/lib_base/LibActivity.java
@@ -0,0 +1,13 @@
+package com.git4u.interview.lib_base;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LibActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lib);
+    }
+}
